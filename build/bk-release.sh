9d648080f732a1d6d06e12b7bc4e149dc97f3d54
Enrico Candino <enrico.candino@suse.com>
2022-11-07 18:15:33 +0100

    test GH action

---
+++ build/bk-release.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+docker run -it \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v $(pwd):/go/src/ui-backend \
+    -w /go/src/ui-backend \
+    -e CGO_ENABLED=1 \
+    -e UI_BUNDLE_URL=$UI_BUNDLE_URL \
+    goreleaser/goreleaser-cross:v1.19.3 --snapshot --rm-dist
